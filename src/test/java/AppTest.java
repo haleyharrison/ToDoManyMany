3364db0b1156bef44f863e9fc8390ba076c1f74e
andrea padgett & haley harrison & ariel spear <student@epicodus.com>
2015-08-31 15:33:47 -0700

    Everything works, integration testing started

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,68 @@
+import org.junit.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.junit.Assert.*;
+import spark.template.velocity.VelocityTemplateEngine;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+public class AppTest extends FluentTest{
+
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Welcome to To Do List");
+  }
+
+  @Test
+  public void categoriesListRenders(){
+    goTo("http://localhost:4567/categories");
+    assertThat(pageSource()).contains("Categories list!");
+  }
+
+  @Test
+  public void categoriesDisplaysList(){
+    Category myCategory = new Category("things to do with nachos");
+    myCategory.save();
+    String catPath = String.format("http://localhost:4567/categories/%d", myCategory.getId());
+    goTo(catPath);
+    assertThat(pageSource()).contains("things to do with nachos");
+  }
+
+  @Test
+  public void addTaskListRenders(){
+    goTo("http://localhost:4567/tasks");
+    assertThat(pageSource()).contains("Task list!");
+  }
+
+  @Test
+  public void addTaskWorks(){
+    Task task = new Task("eat nachos");
+    task.save();
+    String taskPath = String.format("http://localhost:4567/tasks/%d", task.getId());
+    goTo(taskPath);
+    assertThat(pageSource()).contains("eat nachos");
+  }
+
+
+
+
+
+}
